ecf034e9e35de6f76c368c0ff5cd06a9c950664b
jioo <jio_qzn@yahoo.com>
2019-11-16 23:21:07 +0800

    Updated project setup

--- db.sample.php
+++ db.sample.php
@@ -3,7 +3,7 @@
 $db_name = "school";
 $db_server = "127.0.0.1";
 $db_user = "root";
-$db_pass = "rootadmin";
+$db_pass = "";
 
 $db = new PDO("mysql:host={$db_server};dbname={$db_name};charset=utf8", $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
---
+++ setup.sh
@@ -0,0 +1,2 @@
+cp db.sample.php db.php
+cp mobile_app/lib/env.sample.dart mobile_app/lib/env.dart
